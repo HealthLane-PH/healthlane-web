5140ba7b5cc58b8698c6bf18dc95abf3d0bf0f18
Cherry <cherry@bitspacechicago.com>
2025-10-01 01:47:43 +0800

    fix: tailwind text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 export default function Home() {
   return (
     <div className="p-6">
-      <h1 className="text-3xl font-bold text-blue-500">
-        Hello BitSpace with Tailwind!
+      <h1 className="!text-blue-500 text-3xl font-bold">
+        Force Blue
       </h1>
+
+      <h1 className="text-3xl font-bold text-blue-500 underline">
+  Tailwind Blue Works!
+</h1>
+
+
       <p className="text-gray-200">
         If you see colors and styles, Tailwind is working ✅
       </p>
